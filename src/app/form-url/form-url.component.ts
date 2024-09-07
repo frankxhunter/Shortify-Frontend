48a30e805ac79f24ba4b5319befa3e4b52cc40e1
frankxhunter <frankxhunter@gmail.com>
2024-09-07 14:12:53 +0200

    Creado formulario reactivo que valida la url

--- src/app/form-url/form-url.component.ts
+++ src/app/form-url/form-url.component.ts
@@ -1,66 +1,91 @@
 import { NgClass, NgStyle } from '@angular/common';
 import { Component, EventEmitter, inject, Output } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { API_URLS } from '../api-urls';
 import { FetchApiService } from '../services/fetch-api.service';
+import { url } from 'inspector';
+import { error } from 'console';
 
 @Component({
   selector: 'app-form-url',
   standalone: true,
   imports: [FormsModule, ReactiveFormsModule, NgClass, NgStyle],
   templateUrl: './form-url.component.html',
-  styleUrl: './form-url.component.css'
+  styleUrl: './form-url.component.css',
 })
 export class FormUrlComponent {
+  fetchApiService = inject(FetchApiService);
 
-  fetchApiService = inject(FetchApiService)
+  dataSend = '';
 
-  
-  dataSend= "";
-  
   urlForm: FormGroup;
-  
+
   loader = false;
-  
-  urlValue = "https://chatgpt.com/c/c5371a23-6599-4c8a-b82d-649319c2c931"
-  
+
+  errorInput = false;
+
+  // urlValue = 'https://chatgpt.com/c/c5371a23-6599-4c8a-b82d-649319c2c931';
+  urlValue = '';
+
   @Output() fetchEvent = new EventEmitter<string>();
   @Output() errorEvent = new EventEmitter<boolean>();
 
-  constructor(private _form: FormBuilder){
+  constructor(private _form: FormBuilder) {
     this.urlForm = this._form.group({
-      url: ["", [Validators.required, Validators.pattern(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/)]]
-    })
+      url: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/),
+        ],
+      ],
+    });
   }
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.urlForm.patchValue(
-      {
-        url: this.urlValue
-      }
-    )
+    this.urlForm.patchValue({
+      url: this.urlValue,
+    });
   }
 
-  hasErrors(controlName: string, errorType: string){
-    return this.urlForm.get(controlName)?.hasError(errorType) 
-    && this.urlForm.get(controlName)?.touched 
+  hasErrors(controlName: string, errorType: string) {
+    return (
+      this.urlForm.get(controlName)?.hasError(errorType)
+    );
+  }
+  hasErrorsGeneral(controlName: string) {
+    return (
+      this.urlForm.get(controlName)?.errors != null
+    );
   }
 
-fetchData(){
+  fetchData() {
+    if (!this.hasErrorsGeneral("url")) {
+      this.errorInput = false;
 
-  this.fetchApiService.generateShortURL(this.urlValue).subscribe(data => {
-    
-    const shortURLFull = API_URLS.baseURL +"/"+ data.shortUrl
+      this.fetchApiService.generateShortURL(this.urlValue).subscribe(
+        (data) => {
+          const shortURLFull = API_URLS.baseURL + '/' + data.shortUrl;
 
-    //console.log(shortURLFull);
-    this.fetchEvent.emit(shortURLFull)
-
-  }, (error)=>{
-    console.log(error);
-    this.errorEvent.emit(true);
-  })
-
-}
+          //console.log(shortURLFull);
+          this.fetchEvent.emit(shortURLFull);
+        },
+        (error) => {
+          console.log(error);
+          this.errorEvent.emit(true);
+        }
+      );
+    }
+    else{
+      this.errorInput = true;
+    }
+  }
 }
