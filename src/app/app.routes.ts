fddc2a6057f3123d04b63b01fc9f752fe4112853
frankxhunter <frankxhunter@gmail.com>
2024-09-11 17:42:37 +0200

    Creado el prototipo no funcional del login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "user/register", component: RegisterUserComponent}
+];
---
+++ src/app/register-user/register-user.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-user.component.html',
+  styleUrl: './register-user.component.css'
+})
+export class RegisterUserComponent {
+  passwordVisibility = false;
+
+  changePasswordVisibility(){
+    this.passwordVisibility = !this.passwordVisibility;
+  }
+
+}
