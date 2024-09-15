4bcef166aa81eb95e15d5950d81c9411d7d70235
frankxhunter <frankxhunter@gmail.com>
2024-09-15 15:04:15 +0200

    Mejorando el enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
+import {LinksComponent} from  "./links/links.component"
+
 
 export const routes: Routes = [
+    {path: "links", component: LinksComponent},
+    {path: "**", redirectTo: '' }
 ];
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LogginButtonComponent } from './loggin-button/loggin-button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [LogginButtonComponent],
+  imports: [LogginButtonComponent, RouterLink],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
