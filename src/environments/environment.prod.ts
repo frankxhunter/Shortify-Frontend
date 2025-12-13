b215c5f1b9475b3056f8dfcf96328dba5309a117
frankxhunter <frankxhunter@gmail.com>
2025-12-13 02:09:12 +0100

    feat: Update environment configuration with Google Client ID

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    baseUrlBack: "https://shortfy.link"
+    baseUrlBack: "https://shortfy.link",
+    googleClientId: "435102139040-7g3peavcksvsajr0hpmtpuavbvqldh5q.apps.googleusercontent.com"
 }
